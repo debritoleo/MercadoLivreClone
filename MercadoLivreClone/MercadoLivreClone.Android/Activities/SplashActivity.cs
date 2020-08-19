989d686a781714511ef00edaca83acedc617478a
Leonardo de brito <leo_guty11@hotmail.com>
2020-08-18 22:18:14 -0300

    splash screen created

---
+++ MercadoLivreClone/MercadoLivreClone.Android/Activities/SplashActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace MercadoLivreClone.Droid.Activities
+{
+    [Activity(Label = "Mercado Livre Clone", Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
