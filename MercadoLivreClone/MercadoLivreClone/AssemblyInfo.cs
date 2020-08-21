2ab65f9ef5d489114b79e53a8d2bfba502b54a60
Leonardo de brito <leo_guty11@hotmail.com>
2020-08-20 22:58:33 -0300

    Add fonts and menu items.

--- MercadoLivreClone/MercadoLivreClone/AssemblyInfo.cs
+++ MercadoLivreClone/MercadoLivreClone/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+[assembly: ExportFont("FontsFree-Net-Proxima-Nova-Sbold.otf", Alias = "FontProximaSemiBold")]
+[assembly: ExportFont("FontsFree-Net-proxima_nova_reg-webfont.ttf", Alias = "FontProximaRegular")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
